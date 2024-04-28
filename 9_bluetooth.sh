a35490ee97ac6e4666488d12d6e0c67c08d92670
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2024-04-28 12:43:45 +0200

    added bluetooth setup script

---
+++ 9_bluetooth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+
+set -euo pipefail
+
+cd "$(dirname "$0")" || exit
+source check_continue
+source config
+
+cat <<EOF
+ ____  _    _   _ _____ _____ ___   ___ _____ _   _ 
+| __ )| |  | | | | ____|_   _/ _ \ / _ \_   _| | | |
+|  _ \| |  | | | |  _|   | || | | | | | || | | |_| |
+| |_) | |__| |_| | |___  | || |_| | |_| || | |  _  |
+|____/|_____\___/|_____| |_| \___/ \___/ |_| |_| |_|
+EOF
+
+check_continue "installing bluetooth drivers"
+
+sudo pacman -Syy
+sudo pacman -S bluez bluez-utils
+
+sudo systemctl enable bluetooth
+sudo systemctl start bluetooth
