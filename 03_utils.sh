bdbf0efd406cef1700676c4b1a6b701a85c06583
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2024-06-25 23:12:35 +0200

    added chezmoi

--- 03_utils.sh
+++ 03_utils.sh
@@ -19,7 +19,7 @@ check_continue "installing some utils and setting correct time"
 
 
 sudo pacman -Syy
-sudo pacman -S --noconfirm ntp kitty alacritty wget figlet btop xdg-user-dirs code dolphin
+sudo pacman -S --noconfirm ntp kitty alacritty wget figlet btop xdg-user-dirs code dolphin chezmoi
 
 sudo timedatectl set-local-rtc 1
 sudo timedatectl set-ntp true
