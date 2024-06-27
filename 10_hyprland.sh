92c7ecbfb88b639020b3de9b289fbb945c54fa01
Thomas Stichlmair <thomas.stichlmair@gmail.com>
2024-06-27 22:51:37 +0200

    also installing qt5-wayland alongside with hyprland

--- 10_hyprland.sh
+++ 10_hyprland.sh
@@ -15,7 +15,7 @@ cat <<'END_ASCII'
 |_| |_| |_| |_|   |_| \_\_____/_/   \_\_| \_|____/ 
 END_ASCII
 
-sudo pacman -S hyprland dunst qt6-wayland xdg-desktop-portal-hyprland waybar rofi-wayland hyprcursor xdg-desktop-portal-gtk hypridle hyprlock hyprpaper hyprcursor wl-clipboard cliphist dunst waybar polkit-kde-agent
+sudo pacman -S hyprland dunst qt5-wayland qt6-wayland xdg-desktop-portal-hyprland waybar rofi-wayland hyprcursor xdg-desktop-portal-gtk hypridle hyprlock hyprpaper hyprcursor wl-clipboard cliphist dunst waybar polkit-kde-agent
 
 yay -S rose-pine-hyprcursor rose-pine-cursor
 
